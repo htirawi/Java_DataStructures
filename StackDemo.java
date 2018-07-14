bdbe2cccb48284167401754ad8b5d27ac7a2722b
htirawi <hmtirawi@gmail.com>
2018-07-14 21:43:47 +0300

    add main class to test Stack

---
+++ StackDemo.java
@@ -0,0 +1,15 @@
+
+public class StackDemo {
+	
+	public static void main(String[] args) {
+	Stack st = new Stack(3);
+	st.push(10);
+	st.push(11);
+	st.push(12);
+//	st.push(13);
+	
+//	System.out.println(st.pop());
+	st.pop();
+	}
+
+}
