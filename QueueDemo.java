ad41e89b40dbab4bda17398344c129ddaf7acb14
htirawi <hmtirawi@gmail.com>
2018-07-14 21:42:16 +0300

    add main class to test Queue

---
+++ QueueDemo.java
@@ -0,0 +1,19 @@
+
+public class QueueDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Queue q = new Queue(3);
+		
+		q.enQueue(3);
+		q.enQueue(4);
+		q.enQueue(5);
+//		q.enQueue(6);
+		
+		q.display();
+		
+
+	}
+
+}
